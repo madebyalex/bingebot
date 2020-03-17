f9fdc0b6a4fe2576c9af7303b64004ea5c879dd9
Alexander <hello@alexander.works>
2020-03-17 03:06:52 +0200

    Initial Commit

---
+++ Bingebot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bingebot
+//
+//  Created by Alexander on 3/17/20.
+//  Copyright © 2020 Alexander. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
